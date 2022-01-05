40bae89edabf9126afe7f0454e9e747c0edc8f77
gcordner <geoffcordner@gmail.com>
2022-01-05 11:19:55 -0800

    remove social driver plugin

--- plugins/socialdriver-framework/includes/page-builder/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    /*
-    *
-    *	Swift Page Builder
-    *	------------------------------------------------
-    *	Swift Framework v3.0
-    * 	Copyright Swift Ideas 2016 - http://www.swiftideas.com
-    *
-    */
