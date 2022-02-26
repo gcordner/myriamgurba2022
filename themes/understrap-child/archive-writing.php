8b3898717720059436b56d8c07b4ff06d42606e6
Geoff Cordner <geoffcordner@gmail.com>
2022-02-26 08:55:45 -0800

    writing archive and styles

---
+++ themes/understrap-child/archive-writing.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Template Name: Full Width Page
+ *
+ * Template for displaying a page without sidebar even if a sidebar widget is published.
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+
+// if ( is_front_page() ) {
+// 	get_template_part( 'global-templates/hero' );
+// }
+?>
+
+<div class="wrapper p-0  mt-4" id="full-width-page-wrapper">
+<!--mt-4 to comoensate for being covered by fixed-top navbar -->
+	<div class="container-fluid p-0 has-yellow-background-color archive" id="content">
+
+		<div class="row">
+
+			<div class="col-md-12 content-area" id="primary">
+
+				<main class="site-main" id="main" role="main">
+
+				<?php
+				if ( have_posts() ) {
+					?>
+					<header class="page-header">
+						<?php
+						post_type_archive_title( '<h1 class="page-title">', '</h1>' );
+						the_archive_description( '<div class="taxonomy-description">', '</div>' );
+						?>
+					</header><!-- .page-header -->
+                    <div class="container overflow-hidden">
+    <div class="row gx-5 gy-3">
+					<?php
+					// Start the loop.
+					while ( have_posts() ) {
+						the_post();
+
+						/*
+						 * Include the Post-Format-specific template for the content.
+						 * If you want to override this in a child theme, then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'loop-templates/content-writing', get_post_format() );
+					}
+				} else {
+					get_template_part( 'loop-templates/content', 'none' );
+				}
+				?>
+
+				</main><!-- #main -->
+
+			</div><!-- #primary -->
+
+		</div><!-- .row end -->
+
+	</div><!-- #content -->
+
+</div><!-- #full-width-page-wrapper -->
+
+<?php
+get_footer();
