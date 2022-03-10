b17a7e3b9a5871eafc6e41520cbdd60b128c929b
Geoff Cordner <geoffcordner@gmail.com>
2022-03-10 15:43:03 -0800

    plugin updates

--- plugins/coblocks/dist/coblocks-2.asset.php
+++ plugins/coblocks/dist/coblocks-2.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render', 'wp-token-list'), 'version' => 'a0c9392b7fb4d39db71237104e598a21');
+<?php return array('dependencies' => array('lodash', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render', 'wp-token-list'), 'version' => '9d8df830be31ecf31724ef5491f82cb0');
--- plugins/coblocks/dist/js/coblocks-tinyswiper-initializer.asset.php
+++ plugins/coblocks/dist/js/coblocks-tinyswiper-initializer.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'b6a4fb0d14b32822c69abb5e018aff21');
+<?php return array('dependencies' => array(), 'version' => 'd3c0aef94a9a0e8c54f65ab32d9afddd');
