fd4362867f9e54dacf6e50b243559b709bd197ad
gcordner <geoffcordner@gmail.com>
2023-07-28 06:29:13 -0700

    remove featured image from single post; post h2

---
+++ themes/understrap-child/tribe/events/v2/default-template.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * View: Default Template for Events
+ *
+ * Override this template in your own theme by creating a file at:
+ * [your-theme]/tribe/events/v2/default-template.php
+ *
+ * See more documentation about our views templating system.
+ *
+ * @link http://evnt.is/1aiy
+ *
+ * @version 5.0.0
+ */
+
+use Tribe\Events\Views\V2\Template_Bootstrap;
+
+get_header();
+?>
+<div class="wrapper p-0  mt-4 has-yellow-background-color" id="full-width-page-wrapper">
+	<?php
+	echo tribe( Template_Bootstrap::class )->get_view_html();
+	?>
+</div>
+<?php
+get_footer();
